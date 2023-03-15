9522f19cc6bd20c210c70bd4160622f402b4aa7e
Ferdinand Prantl <prantlf@gmail.com>
2023-03-16 00:33:21 +0100

    test: Complete CJS tests

--- test/cjs.test.cjs
+++ test/cjs.test.cjs
@@ -1,9 +1,16 @@
+const test = require('tehanu')(__filename)
+const { strictEqual } = require('assert')
 const {
   getDateLocalePattern, getTimeLocalePattern, getDateTimeLocalePattern
 } = require('datetime-locale-patterns')
 const { setLocalePatterns } = require('datetime-locale-patterns/code')
 
-getDateLocalePattern('cs', 'short')
-getTimeLocalePattern('cs', 'short')
-getDateTimeLocalePattern('cs', 'short', 'short')
-setLocalePatterns({ formats: [], styles: [], patterns: {} })
+test('exports named functions', () => {
+  strictEqual(typeof getDateLocalePattern, 'function')
+  strictEqual(typeof getTimeLocalePattern, 'function')
+  strictEqual(typeof getDateTimeLocalePattern, 'function')
+  getDateLocalePattern('cs', 'short')
+  getTimeLocalePattern('cs', 'short')
+  getDateTimeLocalePattern('cs', 'short', 'short')
+  setLocalePatterns({ formats: [], styles: [], patterns: {} })
+})
